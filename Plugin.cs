655735142b8806ea56434ad5f40e6c24d3fcd429
ibxccc123 <peng.c.danny@gmail.com>
2024-10-31 06:38:31 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanFaceShields.Patches;
+using BepInEx.Configuration;
+
+namespace CleanFaceShields
+{
+    [BepInPlugin("com.penguingreentea.CleanFaceShields", "CleanFaceShields", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        private void Awake()
+        {
+            LogSource = Logger;
+            new FaceShieldPatch().Enable();
+        }
+    }
+}
